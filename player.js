6465015d79ec767f61eab1107924b9e8a1a1f327
Jordan <bigjman1234567@yahoo.com>
2017-03-25 03:34:49 +0000

    wip: trying to get the player to move between two locations

--- player.js
+++ player.js
@@ -1,10 +1,3 @@
-// function playerCreate(x, y) {
-//     player = game.add.sprite(x, y, 'player');
-//     player.enableBody = true;
-//     player.body.collideWorldBounds = true;
-//     player.body.gravity.x = 0;
-//     player.body.gravity.y = 0;
-// }
 var locations = [
     {x: 70, y: 450},
     {x: 230, y: 450}
@@ -12,18 +5,18 @@ var locations = [
 
 function playerCreate(x, y) {
     player = game.add.sprite(x, y, 'player');
-    player.enableBody = true;
+    // player.enableBody = true;
     player.body.collideWorldBounds = true;
     player.body.gravity.x = 0;
     player.body.gravity.y = 0;
 }
 
-function playerUpdate() {
+function playerLocation(x, y) {
     if (cursors.left.isDown) {
-        playerCreate(locations[0].x, locations[0].y);
+        game.add.sprite(locations[0].x, locations[0].y);
     }
     
     if (cursors.right.isDown) {
-        playerCreate(locations[1].x, locations[1].y);
+        game.add.sprite(locations[1].x, locations[1].y);
     }
 }
