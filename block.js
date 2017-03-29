7be2cb5c8b7298fb1b0e805c360f9a77e5f1e0b2
Jordan <bigjman1234567@yahoo.com>
2017-03-29 18:46:13 +0000

    working on blocks spawing randomly

--- block.js
+++ block.js
@@ -4,16 +4,29 @@ Block.preload = function() {
     game.load.spritesheet('block', 'assets/block.png');
 }
 
+Block.locations = {
+    left: {x: 0, y: -50},
+    right: {x: 160, y: 100}
+};
+
+function randBlock() {
+    
+}
+
 Block.create = function() {
-    leftblock = blocks.create(0, 250, 'block');
+    leftblock = blocks.create(0, -50, 'block');
     leftblock.body.immovable = true;
     
     leftblock.body.gravity.y = 25;
-    // leftblock.body.velocity.y = 10;
+    leftblock.body.velocity.y = 250;
     
     rightblock = blocks.create(160, 100, 'block');
     rightblock.body.immovable = true;
     
     rightblock.body.gravity.y = 25;
     // rightblock.body.velocity.y = ;
+}
+
+Block.update = function() {
+    
 }
